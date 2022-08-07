aea9d91eed135001ca4aa63a46f5213a0c81e69e
merleandrade <andrade.merle@gmail.com>
2022-08-07 20:14:04 +0200

    Add axios getTodo request

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {Todo} from "./model/Todo";
+import BoardOverview from "./components/BoardOverview";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const [todos, setTodos] = useState <Todo[]>([]);
+
+useEffect(() => {
+    getAllTodos()
+}, [])
+
+const getAllTodos = () => {
+    axios.get("/api/todo")
+        .then((response) => response.data)
+        .then((data) => setTodos(data))
+}
+
+
+return (
+    <div>
+      <BoardOverview todos = {todos}/>
     </div>
   );
 }
